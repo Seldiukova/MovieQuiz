c26b4a95fa4618b11f97de8743fde9c48eb12484
IrinaSeldiukova <irinaseldiukova@gmail.com>
2025-01-24 22:45:51 -0500

    alert presentation moved to AlertPresenter

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Ирина  Сельдюкова on 1/24/25.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Ирина  Сельдюкова on 1/24/25.
+//
+
+import Foundation
