c8e44c1a26bad07c2ef0319543f526d2fd95c254
IrinaSeldiukova <irinaseldiukova@gmail.com>
2025-01-28 22:33:11 -0500

    add storing data to user defaults

---
+++ MovieQuiz/Models/GameResults.swift
@@ -0,0 +1,8 @@
+//
+//  GameResults.swift
+//  MovieQuiz
+//
+//  Created by Ирина  Сельдюкова on 1/28/25.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by Ирина  Сельдюкова on 1/28/25.
+//
+
+import Foundation
