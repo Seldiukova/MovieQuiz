aad958f6725eb56aafa37ddd63fd3da9cab59933
IrinaSeldiukova <irinaseldiukova@gmail.com>
2025-02-12 16:30:30 -0500

    fix

--- MovieQuiz/Helpers/Date+Extensions.swift
+++ MovieQuiz/Helpers/Date+Extensions.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Date {
-    var dateTimeString: String { DateFormatter.defaultDateTime.string(from: self) }
+    public var dateTimeString: String { DateFormatter.defaultDateTime.string(from: self) }
 }
 
 private extension DateFormatter {
